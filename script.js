7e36b99a3aa7b2a9dc0279405fa8115c51741bbd
Moritz Mitterdorfer <moritzmitterdorfer@Moritzs-MBP.home>
2020-05-27 21:13:13 +0200

    🍭 Added examples

---
+++ script.js
@@ -0,0 +1,47 @@
+/**
+ * Script for My Shop
+ */
+
+/** API key */
+const token = '37a92236-4684-4d83-a482-b6c86f52c813';
+let recon;
+
+let selected = 0;
+
+$(document).ready(() => {
+
+    /** set up SDK */
+    recon = new ReconClient({ token }, () => {
+        console.log('connected...');
+    });
+
+    $('.item').on('click', el => {
+
+        /** increment number of selected items */
+        selected++;
+
+        /** when an item is clicked, add it to recon */
+        recon.addItem(el.target.id);
+
+        /** change properties of item */
+        $(`#${el.target.id}`).css('opacity', '0.3');
+
+        /** show recommendations */
+        recon.recommendAnonymouslyByStorage(items => {
+            console.log(items);
+            $('#recommendations').html('');
+            for(let item of items) {
+                $('#recommendations').append(`<div class="item" id="${item.item}">${item.item}</div>`)
+            }
+        });
+    });
+
+});
+
+function resetUser() {
+    recon.trainModel().then(() => {
+        Cookies.set('userID', Date.now());
+        Cookies.set('items', '');
+        window.location.href = '/';
+    })
+}
